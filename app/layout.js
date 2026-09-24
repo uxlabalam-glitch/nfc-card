6864666360ef7fab2f71e073cdf8ad37a50433fb
uxlabalam-glitch <uxlabalam@gmail.com>
2026-09-25 04:47:04 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Digital Card",
@@ -8,6 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="uz">
+      <head>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
